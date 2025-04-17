1e10013620c2e905b6af56b35b91f923190a511f
WafyPutraPangestu <wafy@raharja.info>
2025-04-17 21:55:37 +0700

    style navbar and home main

---
+++ resources/views/components/navigation.blade.php
@@ -0,0 +1,22 @@
+<header class="fixed top-0 left-0 w-full bg-white z-10">
+    <nav class="container mx-auto">
+        <div class="flex justify-between items-center p-5">
+        <div class="max-w-[50px]">
+            <a href="">
+                <img src="{{ asset('storage/images/WhatsApp Image 2025-04-17 at 16.56.16_d1e96c93.jpg') }}" alt="">
+            </a>
+        </div>
+        <ul class="flex space-x-4">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Services</a></li>
+            <li><a href="#">Contact</a></li>
+            <li><a href="#">Blog</a></li>
+            <li><a href="#">FAQ</a></li>
+        </ul>
+        <div class="">
+            <a href="#" class="btn btn-primary">Login</a>
+        </div>
+    </div>
+    </nav>
+</header>
